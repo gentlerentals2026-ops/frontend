55a990e20b66315d329d9d3a3e0c2f539c70da08
BadareOkiemuteKelvin <badareokiemute2014@gmail.com>
2026-03-26 23:12:30 +0100

    updated site

--- src/constant/apiConstant.js
+++ src/constant/apiConstant.js
@@ -1,9 +1,10 @@
-const isBrowser = typeof window !== "undefined";
-const isLocalhost =
-  isBrowser && (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
+
 
 export const API = {
+
   BASE_URL:
-    process.env.REACT_APP_API_URL ||
-    (isLocalhost ? "http://localhost:9500" : "https://gentlerentals.onrender.com")
+    process.env.NODE_ENV === "production"
+      ? "https://api-gentlerentals.onrender.com"
+      : "http://localhost:9600",
+
 };
