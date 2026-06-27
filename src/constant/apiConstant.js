28ef3590b665900da537a06cd5341ce39d904fbb
BadareOkiemuteKelvin <badareokiemute2014@gmail.com>
2026-06-27 16:28:53 +0100

    Update production domains and ignore secrets

--- src/constant/apiConstant.js
+++ src/constant/apiConstant.js
@@ -1,10 +1,9 @@
 
 
 export const API = {
-
   BASE_URL:
-    process.env.NODE_ENV === "production"
-      ? "https://api-gentlerentals.onrender.com"
-      : "http://localhost:9600",
-
+    process.env.REACT_APP_API_URL ||
+    (process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_PRODUCTION_API_URL || "https://api.gentleevents.com"
+      : "http://localhost:9600")
 };
